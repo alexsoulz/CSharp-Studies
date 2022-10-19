dc56fbb044755251a7b20b08c382c4eb8dbe6030
alexsoulz <aleqsiwav@gmail.com>
2022-10-18 22:43:45 -0300

    atualização do dia

--- CSharpFundamentos/Program.cs
+++ CSharpFundamentos/Program.cs
@@ -1,12 +1,36 @@
 ﻿using CSharpFundamentos.models;
 
+
+//cast - casting
+//int a = Convert.ToInt32("5"); CONVERT É MAIS DAORA
+int a = int.Parse("5");
+Console.WriteLine(a);
+// cast de strin usamos .ToString()
+
+
+
+
+// int a = 5;
+// int b = 10;
+
+// int c = a + b;
+
+// c = +5;
+//c += 5;
+
 //aqui a pessoa passa a existir por uma variavel
-Pessoa pessoa1 = new Pessoa();
+//Pessoa pessoa1 = new Pessoa();
 //todo nome de variavel em camelCase
 
 //definindo os dados dela em seguida
-pessoa1.Nome = "Alex";
-pessoa1.Idade = 20;
+//pessoa1.Nome = "Alex";
+//pessoa1.Idade = 20;
 //usando um metodo
-pessoa1.Apresentar();
+//pessoa1.Apresentar();
+
+//DateTime dataAtual = DateTime.Now.AddYears(5);
+//que viagem
+
+//Console.WriteLine(dataAtual);
+//Console.WriteLine(dataAtual.ToString("dd/mm/yyyy"));
 
